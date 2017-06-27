82b25d4678af2c053af7133bf9db74fb7b593b26
Hridoy Dutta <hridaydutta123@gmail.com>
2017-06-27 13:37:58 +0530

    Added NLP & Compare Polygons

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Constants
+var minIntersectPerc = 30;
+var minSentenceSimilarity = 0.3;
+
 var jsts = require('jsts');
 
 var fs = require('fs');
@@ -9,7 +13,17 @@ const testFolder = '../sync/';
 var turf = require('@turf/turf');
 var geojsonArea = require('@mapbox/geojson-area');
 
+var natural = require('natural');
 
+function calcIntersectPerc(poly1,intersection)
+{
+	// Intersection of two GIS Polygons
+	var area_intersection = geojsonArea.geometry(intersection.geometry);
+	var area_poly1        = geojsonArea.geometry(poly1.geometry);
+
+	// Calculating percentage of overlap of two polygons
+	return ((area_intersection / area_poly1)*100);
+}
 fs.readdir(testFolder, (err, files) => {
   
 	// Find geojson files
@@ -28,18 +42,43 @@ fs.readdir(testFolder, (err, files) => {
 			var poly2 = JSON.parse(fs.readFileSync(testFolder + '/' + fileList[j], 'utf8'));
 			var intersection = turf.intersect(poly1, poly2);
 
-			// Intersection of two GIS Polygons
-			var area_intersection = geojsonArea.geometry(intersection.geometry);
-			var area_poly1        = geojsonArea.geometry(poly1.geometry);
+			// Comparing two text fields using JaroWinklerDistance
+			console.log("Comparing '" + poly1['properties']['TEXT'] + "' and '" + poly2['properties']['TEXT'] + "'");
+			var sentenceSimilarity = natural.JaroWinklerDistance(poly1['properties']['TEXT'],poly2['properties']['TEXT']);
 
-			// Calculating percentage of overlap of two polygons
-			var percent_poly1_covered_by_poly2 = (area_intersection / area_poly1)*100;
-			console.warn("Comparing  " + fileList[i] + " and " + fileList[j]);
-			console.warn("Similarity Percent:" + percent_poly1_covered_by_poly2 + "%\n");
+			if (sentenceSimilarity > minSentenceSimilarity)
+			{	
+				console.warn("sentenceSimilarity " + sentenceSimilarity );
+					
+				var percentCovered = calcIntersectPerc(poly1, intersection);
+				
+				console.warn("Comparing  " + fileList[i] + " and " + fileList[j]);
+				console.warn("Intersection Percent:" + percentCovered + "%\n");
 
-			// Union of two GIS Polygons
-			var union = turf.union(poly1,poly2);
-			console.warn(JSON.stringify(union));
+				switch (true) {
+				    case (percentCovered < 30):
+				    	// Keep the maximum covered area
+				    	console.warn((turf.area(poly1) > turf.area(poly2)) ? JSON.stringify(poly1):JSON.stringify(poly2));
+				        break;
+				    case (percentCovered > 30 && percentCovered < 70):
+				    	// Do something not yet decided
+				        break;
+				    case (percentCovered > 70 && percentCovered < 100):
+				    	// Union of two similar polygons
+				    	console.warn(JSON.stringify(turf.union(poly1,poly2)));
+				        break;
+				    default:
+				        break;
+				}
+				// Union of two GIS Polygons
+				// if(percent_poly1_covered_by_poly2 > minIntersectPerc)
+				// var union = turf.union(poly1,poly2);
+				// console.warn(JSON.stringify(union));
+			}
+			else
+			{
+				console.warn("sentenceSimilarity " + sentenceSimilarity +  " below threshold " + minSentenceSimilarity);
+			}
 
 		}	
 	}
